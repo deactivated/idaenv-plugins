349f91deb3f8177f34bc0e78d4247d00ec69e0d3
mike spindel <mike@spindel.is>
2018-08-13 01:56:19 -0700

    Adapt local_types patch for msgpack

--- idarling/idarling/core/events.py
+++ idarling/idarling/core/events.py
@@ -267,43 +267,26 @@ class LocalTypesChangedEvent(Event):
 
     def __init__(self, local_type):
         super(LocalTypesChangedEvent, self).__init__()
-        if local_type is None:
-            self.local_type = []
-        else:
-            self.local_type = []
-            for t in local_type:
-                if t is not None:
-                    self.local_type.append((
-                        Event.decode_bytes(t[0]) if t[0] is not None else None,
-                        Event.decode_bytes(t[1]) if t[1] is not None else None,
-                        Event.decode_bytes(t[2]) if t[2] is not None else None,
-                    ))
-                else:
-                    self.local_type.append(None)
+        self.local_type = []
+        if local_type is not None:
+            self.local_type.extend(local_type)
 
     def __call__(self):
-        local_type = []
-        for t in self.local_type:
-            if t is not None:
-                local_type.append((
-                    Event.encode_bytes(t[0]) if t[0] is not None else None,
-                    Event.encode_bytes(t[1]) if t[1] is not None else None,
-                    Event.encode_bytes(t[2]) if t[2] is not None else None,
-                ))
-            else:
-                local_type.append(None)
-        missing_ord = len(local_type) - ida_typeinf.get_ordinal_qty(None) + 1
+        missing_ord = (len(self.local_type) -
+                       ida_typeinf.get_ordinal_qty(None) + 1)
         if missing_ord > 0:
             ida_typeinf.alloc_type_ordinals(None, missing_ord)
-        for i, t in enumerate(local_type):
+
+        for i, t in enumerate(self.local_type):
             logger.debug("Processing: %d %s", i, str(t))
             if t is not None:
                 cur_tinfo = ida_typeinf.tinfo_t()
                 cur_tinfo.deserialize(None, t[0], t[1])
                 logger.debug("set_numbered_type ret: %d",
-                             cur_tinfo.set_numbered_type(None, i+1, 0, t[2]))
+                             cur_tinfo.set_numbered_type(None, i + 1, 0, t[2]))
             else:
-                ida_typeinf.del_numbered_type(None, i+1)
+                ida_typeinf.del_numbered_type(None, i + 1)
+
         ida_kernwin.request_refresh(ida_kernwin.IWID_LOCTYPS)
 
 
